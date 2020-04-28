d9c08257b2ca327fb187c40eddc4d50d72a95d09
swg <suweigao1@gmail.com>
2020-04-28 18:58:19 +0800

    test

--- Lab2/Server.h
+++ Lab2/Server.h
@@ -13,34 +13,30 @@ using namespace std;
 #include <arpa/inet.h>
 #include<pthread.h>
 #include<queue>
+#include<assert.h>
 class Server{
 	public:
 		sockaddr_in server_addr;
 		int server_port;
 		int listenfd;
 		Server(){}
-		Server (int port){
-			listenfd = socket(AF_INET,SOCK_STREAM,0); 
-			 int reuse=1;
-			 setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+		Server (int port,string ip){
 			 server_addr.sin_family = AF_INET;  // 协议族，在socket编程中只能是AF_INET。
 			 server_addr.sin_addr.s_addr = htonl(INADDR_ANY);          // 任意ip地址。
-			 //servaddr.sin_addr.s_addr = inet_addr("118.89.50.198"); // 指定ip地址。
-			 server_addr.sin_port = htons(8888);  // 指定通信端口。
-			   if (bind(listenfd,(struct sockaddr *)&server_addr,sizeof(server_addr)) != 0 ){
-			   	 perror("bind failed!!\n"); 
-			   	 close(listenfd); 
-			   }
-			    if (listen(listenfd,10) != 0 ) {
-			     perror("listen failed!\n"); 
-			     close(listenfd);
-			     }
+			 //server_addr.sin_addr.s_addr = htonl(inet_addr("127.0.0.1")); // 指定ip地址。
+			 server_addr.sin_port = htons(port);  // 指定通信端口。
+			listenfd = socket(AF_INET,SOCK_STREAM,0); 
+			assert(listenfd>=0);
+			 int reuse=1;
+			 setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+			 assert (bind(listenfd,(struct sockaddr *)&server_addr,sizeof(server_addr)) >= 0 );
+			 assert (listen(listenfd,5) >= 0 );
 		}
 		int acc_conn(){
-			sockaddr_in client_addr;
-			int socklen=sizeof(client_addr);
-			// multiple accept!!!!!!!!!!!
-			int client_sockfd=accept(this->listenfd,(struct sockaddr *)&client_addr,(socklen_t*)&socklen);
+			struct sockaddr_in client_addr;
+			//memset(&client_addr,0,sizeof(client_addr));
+			socklen_t socklen=sizeof(sockaddr_in);
+			int client_sockfd=accept(this->listenfd,(sockaddr *)&client_addr,&socklen);
 			cout<<"client connected "<<inet_ntoa(client_addr.sin_addr)<<'\n';
 			return client_sockfd;
 		}
@@ -49,40 +45,31 @@ class Server{
 			 int mark=0;
 			 if (recv(client_sockfd,msg,sizeof(msg),0)>=0) {   // 接收客户端的请求报文。
 			 		mark=1;
-			 		cout<<"request received\n";
+			 		//cout<<"request received\n";
     				//cout<<"request received\n"<<msg<<'\n';
     				str=msg;
     			}
     			//TODO: handle the state code
     			return mark;
 		}
-		int send_response(int client_sockfd, char* filename){
-			int flag=0;//0 for 200  1 for 404
+		int send_response(PForm task,int client_sockfd, char* filename){//add headers
 			string header="";
-			char body[1024];
 			char msg[2048];
-			//body
-			ifstream infile(filename);
-			if(!infile) flag=1;//no such file   404
-			infile.seekg(0,ios::end);
-			int len=infile.tellg();
-			infile.seekg(0,ios::beg);
-			infile.read(body,sizeof(body));
-			infile.close();
+
 			//header
-			if(flag==0){
+			if(task.code==200){
 				header+="HTTP/1.1 200 OK\n";
+				}
+			else if(flag==501){
+				header+="HTTP/1.1 501 Not Implemented\n";
 			}
-			else if(flag==1){
-				header+="HTTP/1.1 404 NOT FOUND\n";
+			else {
+				header+="HTTP/1.1 404 Not Found\n";
 			}
-			header+="Server: SWG's Server\n";
-			header+="Content-length: "+to_string(strlen(body))+"\n";
-			header+="Content-type: text/html\n\n";
-			sprintf(msg,"%s%s",header.data(),body);
-			cout<<"发送!!\n";
-			//delete it later  
-			close(listenfd);
+				header+="Server: SWG's Server\n";
+				header+="Content-length: "+to_string(strlen(body))+"\n";
+				header+="Content-type: text/html\n\n";
+				sprintf(msg,"%s%s",header.data(),task.content.data());
 			return send(client_sockfd,msg,strlen(msg),0);
 		}
 };
--- Lab2/main.c
+++ Lab2/main.c
@@ -1,5 +1,6 @@
 #include"Server.h"
 struct PForm{
+	int code;
 	int sockfd;
 	int method;//0 for GET  1 for POST
 	char* filename;
@@ -9,7 +10,8 @@ queue<PForm>pipeline;
 pthread_mutex_t mutex;
 pthread_cond_t cond;
 pthread_t* pthread_pool;
-int pthread_num,port;
+int pthread_num,port=8880;
+string ip="127.0.0.1";
 Server* server;
 void* run(void *){
 	while(1){
@@ -17,30 +19,52 @@ void* run(void *){
 		while(pipeline.empty()){
 			pthread_cond_wait(&cond,&mutex);
 		}
+		cout<<"发送!!\n";
 		PForm task=pipeline.front();
 		pipeline.pop();
 		pthread_mutex_unlock(&mutex);
-		if(task.method==0){//GET
-			server->send_response(task.sockfd,task.filename);
-		}
-		else if(task.method==1){//POST
-			ofstream outfile(task.filename,ios::app);
-			outfile<<task.content;
-			outfile.close();
-			server->send_response(task.sockfd,task.filename);
-		}
+//		if(task.method==0){//GET
+		server->send_response(tasl.method,task.sockfd,task.filename);
+//		}
+//		else if(task.method==1){//POST
+//			ofstream outfile(task.filename,ios::app);
+//			outfile<<task.content;
+//			outfile.close();
+//			server->send_response(task.method, task.sockfd,task.filename);
+//		}
 	}
+}
+PForm parser(string buffer){
+	PForm task;
+	//check method
+	string filename="";
+	int get_pos=buffer.find("GET");
+	int post_pos=buffer.find("POST");
+	if(get_pos!=string::npos){
+		task.method=0;
+		filename=buffer.substr(get_pos+5,buffer.find("HTTP")-get_pos-6);
+		task.filename=filename.c_str();
+	}
+	else if(post_pos!=string::npos){
+		task.method=1;
+		filename=buffer.substr(post_pos+5,buffer.find("HTTP")-post_pos-6);
+		task.filename=filename.c_str();
+	}
+	else{
+		task.method=-1;
+		// below to send bad
+		task.filename="501.html";
+	}
+	return task;
 }
 int main(int argc, char *argv[]){
 	pthread_mutex_init(&mutex,NULL);
 	cond = PTHREAD_COND_INITIALIZER;
-	//string ip=argv[2];
-	//port=atoi(argv[4]);
-	port=8888;
+	string ip=argv[2];
+	port=atoi(argv[4]);
 	if(argc==7)
 		pthread_num=atoi(argv[6]);
-	else pthread_num=4;
-	
+	else pthread_num=1;
 	pthread_pool=new pthread_t[pthread_num];
 	for(int i=0;i<pthread_num;i++)
 	{
@@ -48,26 +72,29 @@ int main(int argc, char *argv[]){
 		pthread_detach(pthread_pool[i]);//分离线程
 	}
 	
-	server=new Server(port);//set port, the server starts listening
+	server=new Server(port,ip);//set port, the server starts listening
 
 	string buffer="";
-	int client_sockfd;
+	int client_sockfd=-1;
 	while(1){//message from client){//receive client's requests
-		//THREAD PROCESS: (200OK  CONSIDERED ONLY)
 		client_sockfd=server->acc_conn();
-		//PForm task=server.parser(buffer);
-		PForm task;
-		task.sockfd=client_sockfd;
-		task.filename="index.html";
-		task.method=0;
-		task.content="";
-//		task.filename="Post_show";
-//		task.method=1;
-//		task.content="postpostpost\n";
-		pthread_mutex_lock(&mutex);
-		pipeline.push(task);
-		//wake up
-		pthread_cond_signal(&cond);
-		pthread_mutex_unlock(&mutex);
+		assert(client_sockfd>=0);
+		if(server->recv_request(buffer,client_sockfd)){
+			PForm task=parser(buffer);
+			task.sockfd=client_sockfd;
+//			task.filename="index.html";
+//			task.method=0;
+//			task.content="";
+	//		task.filename="Post_show";
+	//		task.method=1;
+	//		task.content="postpostpost\n";
+			pthread_mutex_lock(&mutex);
+			pipeline.push(task);
+			//wake up
+			pthread_cond_signal(&cond);
+			pthread_mutex_unlock(&mutex);
+			}
+			else break;
 	}
+	close(server->listenfd);
 }
